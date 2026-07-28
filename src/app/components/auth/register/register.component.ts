65cfb3b514382a5254bedf525a4c98a8aa27e960
rahulroman <rahulshinde2144@gmail.com>
2026-07-28 22:44:08 +0530

    added ap service

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,14 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { userRegister } from '../../../models/authModels/authModels'
-import {  FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../../services/auth.service';
+import { ApiServiceService } from '../../../services/api.service';
 
 @Component({
   selector: 'app-register',
-  standalone : true,
-  imports: [FormsModule,CommonModule],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
@@ -16,39 +17,44 @@ export class RegisterComponent {
 
   private route = inject(Router);
   private authService = inject(AuthService);
+  private apiService = inject(ApiServiceService)
 
-  User : userRegister = {
+  User: userRegister = {
+    FullName: '',
+    email: '',
+    Password: ''
+  };
 
-    FullName : '',
-    email  : '',
-    Password : ''
+  onRegister() {
 
-  } ;
+    //    this.isLoading = true;
 
-  onRegister(){
+    this.authService.onRegister(this.User).subscribe({
+      next: (res) => {
+        //  this.notificationService.showSuccess("Logged in successfully");
+        //     this.route.navigate(['/dashboard']);
+        //    this.isLoading = false;
+        //  this.loadingService.hide();
 
-  //    this.isLoading = true;
-
-   this.authService.onRegister(this.User).subscribe({
-     next : (res) => { 
-      //  this.notificationService.showSuccess("Logged in successfully");
-      //     this.route.navigate(['/dashboard']);
-      //    this.isLoading = false;
-      //  this.loadingService.hide();
-
-     },
-      error : (error) => {
-      //     this.notificationService.showError("Login failed: " + error.message);
-      //    this.isLoading = false;
-      //  this.loadingService.hide();
+      },
+      error: (error) => {
+        //     this.notificationService.showError("Login failed: " + error.message);
+        //    this.isLoading = false;
+        //  this.loadingService.hide();
       }
-   });
+    });
   }
 
 
 
 
-  change (){
- this.route.navigate(["auth/login"]);
+  change() {
+    this.route.navigate(["auth/login"]);
   }
+
+
+
+
+
+
 }
